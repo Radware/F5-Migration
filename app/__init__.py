26da478fd9380d37e74596faa5cfa648b970d734
Valentine Gutkin <admin@networklabs.info>
2019-09-01 17:57:40 +0300

    Update __init__.py
    + bug fix in local support

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
-from flask import Flask
+try:
+	from flask import Flask
+	app = Flask(__name__)
+except Exception as e:
+	print("No flask installed!")
 
-app = Flask(__name__)
+try:
+	from app import route
+except Exception as e:
+	print ("route import failed!")
+
 
 #from app import routes
-from app import route
 #from app import lp_6_12_to_lpng
