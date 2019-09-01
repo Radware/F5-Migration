6758e2c3f48d8d9b7c2ee6f8e9b62a90dd67baee
Valentine Gutkin <admin@networklabs.info>
2019-09-01 17:56:58 +0300

    Update local_runner.py
    + support multiple files

--- local_runner.py
+++ local_runner.py
@@ -1,7 +1,10 @@
 import sys
-from f5_mig import *
+from app.f5_mig import *
 
 if len(sys.argv)>1:
-	fun_f5_mig(sys.argv[1], 'local', 0)
+	file_names=""
+	for i in sys.argv[1:]:
+		file_names=file_names+"$val$"+i
+	fun_f5_mig(file_names[5:], 'local', 0)
 else:
 	fun_f5_mig(input("Please provide path to config files\n"), 'local', 0)
