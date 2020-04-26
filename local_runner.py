c97e1b42b8f23aa8a79a69fbbca5588b12322628
Valentine Gutkin <admin@networklabs.info>
2020-04-26 13:58:57 +0300

    Update local_runner.py
    + fix windows invocation bug

--- local_runner.py
+++ local_runner.py
@@ -7,4 +7,4 @@ if len(sys.argv)>1:
 		file_names=file_names+"$val$"+i
 	fun_f5_mig(file_names[5:], 'local', 0)
 else:
-	fun_f5_mig(input("Please provide path to config files\n"), 'local', 0)
+	fun_f5_mig(input("Please provide path to config files\n").replace('"',''), 'local', 0)
